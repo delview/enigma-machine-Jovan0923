3c12e821665e55cb2abd2f379e7aa20dca454153
Jovan0923 <jovanb331@deltalearns.ca>
2025-03-06 17:24:12 +0000

    Added the Pseudocode for the program. I forgot to commit before I started to code so theres a bit of code.

---
+++ main.py
@@ -0,0 +1,17 @@
+# Greet user and explain what an enigma machine is and what cipher its using
+print("Welcome to the Enigma Machine!")
+print("The Enigma Machine was a cipher machine used by the Germans during World War II.")
+print("This program uses the Atbash Cipher to encrypt and decrypt messages.")
+# Create a dictionary with the alphabet and its corresponding number
+
+# Ask user if they want to encrypt, decrypt, or quit
+choice = input("Would you like to [e]ncrypt, [d]ecrypt, or [q]uit? ")
+# If they want to encrypt, ask for a message
+
+# Create function to encrypt message
+def encrypt_message()
+# If they want to decrypt, ask for a message 
+
+# Create function to decrypt message
+
+# If they want to quit, say goodbye
