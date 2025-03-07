26cd46b3b37a8e65809c7d8fb2a6c306ba484361
Jovan0923 <jovanb331@deltalearns.ca>
2025-03-07 19:34:32 +0000

    I added sleep timers to make the text look neater and added import time at the top of the code.

--- main.py
+++ main.py
@@ -1,26 +1,35 @@
+import time
 # Greet user and explain what an enigma machine is and what cipher its using
 print("Welcome to the Enigma Machine!")
+time.sleep(1)
 print("The Enigma Machine was a cipher machine used by the Germans during World War II.")
+time.sleep(1)
 print("This program uses the Atbash Cipher to encrypt and decrypt messages.")
+time.sleep(1)
 # Create a dictionary with the alphabet and its corresponding number
-sipher = {"a": "z", "b": "y", "c": "x", "d": "w", "e": "v", "f": "u", "g": "t", "h": "s", "i": "r", "j": "q", "k": "p", "l": "o", "m": "n", "n": "m", "o": "l", "p": "k", "q": "j", "r": "i", "s": "h", "t": "g", "u": "f", "v": "e", "w": "d", "x": "c", "y": "b", "z": "a"}
+cipher = {"a": "z", "b": "y", "c": "x", "d": "w", "e": "v", "f": "u", "g": "t", "h": "s", "i": "r", "j": "q", "k": "p", "l": "o", "m": "n", "n": "m", "o": "l", "p": "k", "q": "j", "r": "i", "s": "h", "t": "g", "u": "f", "v": "e", "w": "d", "x": "c", "y": "b", "z": "a"}
 # Ask user if they want to encrypt, decrypt, or quit
 choice = input("Would you like to [e]ncrypt, [d]ecrypt, or [q]uit? ")
 # If they want to encrypt, ask for a message
 if choice == "e":
     message = input("What message would you like to encrypt? ")
-    print("Encrypting message... this could take days....... im kidding")
+    print("Encrypting message...")
+    time.sleep(2)
+    print("this could take days.......")
+    time.sleep(2)
+    print("im kidding.......")
 # Create function to encrypt message
 def encrypt_message(message):
         encrypted_message = ""
         for letter in message:
-            if letter in sipher:
-                encrypted_message += sipher[letter]
+            if letter in cipher:
+                encrypted_message += cipher[letter]
             else:
                 encrypted_message += letter
         return encrypted_message
 # Call the encrypt_message function and print the result
 encrypted_message = encrypt_message(message)
+time.sleep(2)
 print("Encrypted message:", encrypted_message)
 # If they want to decrypt, ask for a message 
 
