87f2d15482e9a61dfb540de9f4d4233650c6dc05
Jovan0923 <jovanb331@deltalearns.ca>
2025-03-10 20:25:19 +0000

    If you type a word with caps in it the caps would not cypher so I added caps to the dyctionary so they can be cyphered aswell.

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ time.sleep(1)
 print("This program uses the Atbash Cipher to encrypt and decrypt messages.")
 time.sleep(1)
 # Create a dictionary with the alphabet and its corresponding number
-cipher = {"a": "z", "b": "y", "c": "x", "d": "w", "e": "v", "f": "u", "g": "t", "h": "s", "i": "r", "j": "q", "k": "p", "l": "o", "m": "n", "n": "m", "o": "l", "p": "k", "q": "j", "r": "i", "s": "h", "t": "g", "u": "f", "v": "e", "w": "d", "x": "c", "y": "b", "z": "a"}
+cipher = {"a": "z", "b": "y", "c": "x", "d": "w", "e": "v", "f": "u", "g": "t", "h": "s", "i": "r", "j": "q", "k": "p", "l": "o", "m": "n", "n": "m", "o": "l", "p": "k", "q": "j", "r": "i", "s": "h", "t": "g", "u": "f", "v": "e", "w": "d", "x": "c", "y": "b", "z": "a", "1": "0", "2": "9", "3": "8", "4": "7", "5": "6", "6": "5", "7": "4", "8": "3", "9": "2", "0": "1", "A": "Z", "B": "Y", "C": "X", "D": "W", "E": "V", "F": "U", "G": "T", "H": "S", "I": "R", "J": "Q", "K": "P", "L": "O", "M": "N", "N": "M", "O": "L", "P": "K", "Q": "J", "R": "I", "S": "H", "T": "G", "U": "F", "V": "E", "W": "D", "X": "C", "Y": "B",}
 # Ask user if they want to encrypt, decrypt, or quit
 choice = input("Would you like to [e]ncrypt, [d]ecrypt, or [q]uit? ")
 # If they want to encrypt, ask for a message
